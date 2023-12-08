71baa767c2c0d7a537176629252d76d5f9326904
subhashishjungshah <subhashishjungshah@gmail.com>
2023-12-08 09:43:47 +0545

    added cmd for producer

--- producer.js
+++ producer.js
@@ -1,4 +1,10 @@
 const { kafka } = require("./client");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 async function init() {
   const producer = kafka.producer();
@@ -7,17 +13,24 @@ async function init() {
   await producer.connect();
   console.log("Producer connected successfully!");
 
-  await producer.send({
-    topic: "pathau-rider-updates",
-    messages: [
-      {
-        partition: 0,
-        key: "location-update",
-        value: JSON.stringify({ name: "Subhashish", location: "SOUTH" }),
-      },
-    ],
+  rl.setPrompt(">");
+  rl.prompt();
+
+  rl.on("line", async function (line) {
+    const [name, location] = line.split(" ");
+    await producer.send({
+      topic: "pathau-rider-updates",
+      messages: [
+        {
+          partition: location.toLowerCase() === "south" ? 0 : 1,
+          key: "location-update",
+          value: JSON.stringify({ name, location }),
+        },
+      ],
+    });
+  }).on("close", async () => {
+    await producer.disconnect();
   });
-  await producer.disconnect();
 }
 
 init();
